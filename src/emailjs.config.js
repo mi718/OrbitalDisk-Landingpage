304968c39bdb1f056973ff7a14de95b4091134e1
mi718 <micael.staeubli@gmail.com>
2026-01-13 23:07:23 +0100

    waitlist

---
+++ src/emailjs.config.js
@@ -0,0 +1,4 @@
+// Add your EmailJS credentials here
+export const EMAILJS_SERVICE_ID = "service_rqorsq5";
+export const EMAILJS_TEMPLATE_ID = "template_7vk3yq7";
+export const EMAILJS_USER_ID = "nYg_-5gBbAtQbYPWr";
